b3e5513a50e3811d99f3c7e1e8fa7f1de0bbbe6c
Mike Oswell <oswell@origamilogic.com>
2016-05-30 15:28:40 -0700

    Add a Get_Roles(name) function to allow fetching of all roles.

--- onelogin.go
+++ onelogin.go
@@ -58,25 +58,42 @@ func (o *OneLogin) Get_UserByUsername(username string)(*OneLoginUser, error) {
     return &get_user_response.Data[0], nil
 }
 
-func (o *OneLogin) Get_Role_Id(role string)(int, error) {
-    logger.Debugf("Requesting role %s", role)
+// Get all roles, or specify a name for a single role.
+// Get_Roles("")           => Get all roles
+// Get_Roles("operations") => Get operations role
+func (o *OneLogin) Get_Roles(name string)([]OneLoginRole, error) {
+    logger.Debugf("Requesting roles")
 
     oauth_token, err := o.Get_OAuth_Token(); if err != nil {
-        return -1, ErrorOcurred(err)
+        return nil, ErrorOcurred(err)
     }
 
     url := o.createUrl(ROLE_GET_ROLES)
     headers := o.headers(fmt.Sprintf("bearer:%s", oauth_token))
-    params := map[string]string{ "name": role }
+    params := map[string]string{}
+
+    if name != "" {
+        params = map[string]string{ "name": name }
+    }
 
     get_role_response := GetRoleResponse{}
 
     client := HttpClient{Url: url, Headers: headers, Params: params}
     _, err = client.Request("GET", nil, &get_role_response) ; if err != nil {
-        return -1, ErrorOcurred(err)
+        return nil, ErrorOcurred(err)
     }
 
     roles := get_role_response.Data
+
+    return roles, nil
+}
+
+func (o *OneLogin) Get_Role_Id(role string)(int, error) {
+    logger.Debugf("Requesting role %s", role)
+
+    roles, err := o.Get_Roles(role) ; if err != nil {
+        return -1, ErrorOcurred(err)
+    }
     if len(roles) != 1 {
         return -1, errors.New(fmt.Sprintf("Found %d roles matching %s, expected only 1.", len(roles), role))
     }
